7e34caed798ec3661f4a852e2e06289f13611b7f
jonpfluger <jonpfluger718@gmail.com>
2024-01-30 07:58:41 -0600

    new about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <img id='headshot' src={headshot} alt="Recent picture of Jon Pfluger" />
             <div id='bio'>
                 <h2>Full Stack Web Developer</h2>
-                <p>Hi, I'm Jon! I love to code and learn new things. I've been a computer person my entire life. I currently work in retail sales for Prime Communications, an authorized retailer for AT&T. When I began working at AT&T my knowledge of mobile technology skyrocketed. Now I enjoy designing websites that work with both PC and mobile platforms. I am excited to make a career change into the coding world. I have always been passionate about user-friendly design for software applications. I'm a firm believer that anyone should be able to use your application, even those who are not "tech-savvy".</p>
+                <p>Full-stack developer leveraging background in mobile technology to build professional and intuitive user experiences on the web. Recently graduated from a full-stack coding boot camp from the University of Wisconsin with a 4.0 GPA. Post graduation, was hired as the TA for the next class in the boot camp. Excellent problem solver, very organized, success with project management and team collaboration.</p>
             </div>
         </div>
     )
