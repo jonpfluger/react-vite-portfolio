736d1dd3ea39ab8ee55bb52ea1e8e7511fd399dc
jonpfluger <jonpfluger718@gmail.com>
2024-02-05 20:00:42 -0600

    new About Me description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <img id='headshot' src={headshot} alt="Recent picture of Jon Pfluger" />
             <div id='bio'>
                 <h2>Full Stack Web Developer</h2>
-                <p>Full-stack developer leveraging background in mobile technology to build professional and intuitive user experiences on the web. Recently graduated from a full-stack coding boot camp from the University of Wisconsin with a 4.0 GPA. Post graduation, was hired as the TA for the next class in the boot camp. Excellent problem solver, very organized, success with project management and team collaboration.</p>
+                <p>Full-stack developer leveraging background in mobile technology to build professional and intuitive user experiences on the web. Recently graduated from a full-stack coding boot camp from the University of Wisconsin with a 4.0 GPA. Post graduation, was hired as the TA for the next class in the boot camp. Excellent problem solver. Success with an agile team in a CI/CD pipeline.</p>
             </div>
         </div>
     )
