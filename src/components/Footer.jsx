d813a5245b5067817feb7f8822b877cd3224366e
Jon Pfluger <jonpfluger718@gmail.com>
2024-12-30 16:20:46 -0600

    updated linkedin link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="flex-row px-1">
             <div>
                 <a href="https://github.com/jonpfluger" target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a>
-                <a href="https://www.linkedin.com/in/jonathanjeffreypfluger/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+                <a href="https://www.linkedin.com/in/jpwebdev/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
                 <a href="https://stackoverflow.com/users/19670475/jon-pfluger" target="_blank" rel="noreferrer"><i className="bi bi-stack-overflow"></i></a>
             </div>
 
